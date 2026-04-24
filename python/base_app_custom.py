59d1963cd7b52816dd01a2a6cd74c592d481159a
Keelan Byrne <kbyrne@interactivebrokers.co.uk>
2026-04-24 11:15:50 +0100

    create second base app as template

---
+++ python/base_app_custom.py
@@ -0,0 +1,40 @@
+from ibapi.client import *
+from ibapi.wrapper import *
+
+import threading, time
+
+class BaseApp(EClient, EWrapper):
+    def __init__(self, host="localhost", port=7497, client_id=0):
+        self.host = host
+        self.port = port
+        self.client_id = client_id
+
+        EClient.__init__(self, wrapper=self)
+        
+        self.connect(host, port, client_id)
+        
+        thread = threading.Thread(target=self.run)
+        thread.start()
+
+        # Wait for nextValidId callback
+        start = time.time()
+        while not hasattr(self, "order_id"):
+            if time.time() - start > 5:
+                print("Timeout waiting for nextValidId!")
+                self.disconnect()
+                exit(1)
+
+    def nextValidId(self, orderId):
+        self.order_id = orderId
+
+    def nextId(self):
+        self.order_id += 1
+        return self.order_id
+    
+    def error(self, reqId, errorTime, errorCode, errorString, advancedOrderRejectJson=""):
+        print(reqId, errorTime, errorCode, errorString, advancedOrderRejectJson)
+
+    def connectionClosed(self):
+        self.connect(self.host, self.port, self.client_id)
+
+    # Add relevant wrapper callbacks below
